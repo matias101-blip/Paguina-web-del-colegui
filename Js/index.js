cdd31025b16455578ec1097d30544e194a24e41c
Thehunter101 <murciamatias101@gmail.com>
2022-01-12 22:07:58 -0500

    Paguina web completa y estable, faltan uno que otro cambio

---
+++ Js/index.js
@@ -0,0 +1,89 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.hero',{
+        duration:2000,
+        origin:'top',
+        distance:'100px'
+    });
+
+    sr.reveal('.Quienes-somos ',{
+        duration:2500,
+        origin:'bottom',
+        distance:'100px'
+    });
+
+    sr.reveal('.carrera',{
+        duration:3000,
+        origin:'top',
+        distance:'100px'
+    });
+
+    sr.reveal('.info',{
+        duration:2000,
+        origin:'right',
+        distance:'200px'
+    });
+
+    sr.reveal('.img-2',{
+        duration:4000,
+        rotate:{
+            x: 1,
+            y: 180,
+        }
+    });
+
+    sr.reveal('.img-1',{
+        duration:2000,
+        origin:'left',
+        distance:'200px'
+    });
+
+    sr.reveal('.img-3',{
+        duration:2000,
+        origin:'rigth',
+        distance:'200px'
+    });
+
+    sr.reveal('.requisitos',{
+        duration:2000,
+        origin:'left',
+        distance:'200px'
+    });
+
+    sr.reveal('.text-re',{
+        duration:2000,
+        origin:'bottom',
+        distance:'200px'
+    });
+
+    sr.reveal('.vid1',{
+        duration:3350,
+        rotate:{
+            x: 1,
+            y: 180,
+        }
+    });
+
+    sr.reveal('.vid-1',{
+        duration:4000,
+        origin:'bottom',
+        distance:'200px'
+    });
+
+    sr.reveal('.vid-2',{
+        duration:4000,
+        origin:'top',
+        distance:'200px'
+    });
+
+    sr.reveal('.contac-1',{
+        duration:2000,
+        origin:'left',
+        distance:'200px'
+    });
+
+    sr.reveal('.contac-2',{
+        duration:2000,
+        origin:'rigth',
+        distance:'200px'
+    });
